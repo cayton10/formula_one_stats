a03dbf610edfbb8bbee69d26d81c6aaa3cdbb359
Obi-Wan <cayton10@live.marshall.edu>
2022-03-18 20:28:27 -0400

    All three tiers work in dockerized env

--- server/app.ts
+++
@@ -1,8 +0,0 @@
-import express, { Request, Response, NextFunction } from 'express';
-
-const app = express();
-const port = 3000;
-
-app.listen(port, () => {
-    console.log(`Timezones by location application is running on port ${port}.`);
-});
